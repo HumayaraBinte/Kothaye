1e92860f404c7512a65089de5ed65d50f5782956
unknown <mousfiqul.khan@northsouth.edu>
2019-12-13 17:59:01 +0600

    Connected to firebase

--- app/src/main/java/com/example/kothayebhai/SignUpActivity.java
+++ app/src/main/java/com/example/kothayebhai/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.kothayebhai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,19 +8,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText signUpEmailEditText, signUpPasswordEditText;
     private TextView signInTextView;
     private Button signUpButton;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        this.setTitle("Sign up Activity");
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        progressBar = findViewById(R.id.progressbarId);
         signUpEmailEditText = findViewById(R.id.signUpEmailEditTextId);
         signUpPasswordEditText = findViewById(R.id.signUpPasswordEditTextId);
         signInTextView = findViewById(R.id.signInTextViewId);
@@ -33,7 +48,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.signUpButtonId:
-
+                userRegister();
                 break;
 
             case R.id.signInTextViewId:
@@ -41,5 +56,53 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                 startActivity(intent);
                 break;
         }
+    }
+
+    private void userRegister() {
+
+        String email = signUpEmailEditText.getText().toString().trim();
+        String password = signUpPasswordEditText.getText().toString().trim();
+
+        if(email.isEmpty())
+        {
+            signUpEmailEditText.setError("Enter an email address");
+            signUpEmailEditText.requestFocus();
+            return;
+        }
+
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            signUpEmailEditText.setError("Enter a valid email address");
+            signUpEmailEditText.requestFocus();
+            return;
+        }
+
+        //checking the validity of the password
+        if(password.isEmpty())
+        {
+            signUpPasswordEditText.setError("Enter a password");
+            signUpPasswordEditText.requestFocus();
+            return;
+        }
+
+        if(password.length()<6){
+            signUpPasswordEditText.setError("Minimum lengh of a password should be 6");
+            signUpPasswordEditText.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Faced error during registration", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
