819f8893e867b6625bcb2aef69a52d420a66ca9f
unknown <mousfiqul.khan@northsouth.edu>
2020-01-23 22:42:34 +0600

    Design of Home Fragement

--- app/src/main/java/com/example/kothayebhai/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/kothayebhai/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.kothayebhai.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +14,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.kothayebhai.FindFreelancerActivity;
+import com.example.kothayebhai.FindWorkActivity;
 import com.example.kothayebhai.R;
 
 public class HomeFragment extends Fragment {
@@ -23,11 +27,29 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        final Button btn = root.findViewById(R.id.freelanceButtonId);
+        final Button btn1 = root.findViewById(R.id.workButtonId);
+        final Button btn2 = root.findViewById(R.id.placeButtonId);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), FindFreelancerActivity.class));
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), FindWorkActivity.class));
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), null));
             }
         });
         return root;
