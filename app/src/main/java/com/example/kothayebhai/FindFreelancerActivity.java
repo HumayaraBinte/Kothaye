b8c4ecfbe64027e953ebf5ba1a2762338bd3fa0c
unknown <mousfiqul.khan@northsouth.edu>
2020-02-04 00:04:52 +0600

    Freelancer Activity template created

--- app/src/main/java/com/example/kothayebhai/FindFreelancerActivity.java
+++ app/src/main/java/com/example/kothayebhai/FindFreelancerActivity.java
@@ -1,14 +1,80 @@
 package com.example.kothayebhai;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class FindFreelancerActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ImageButton selectPostImage;
+    private Button updatePostButton;
+    private EditText postDescription;
+    private static final int Gallery_pick=1;
+    private Uri ImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_freelancer);
+
+        selectPostImage = findViewById(R.id.imageButton4);
+        updatePostButton = findViewById(R.id.postButtonId);
+        postDescription = findViewById(R.id.editText3);
+
+        mToolbar = findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
+
+        selectPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, Gallery_pick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==Gallery_pick && resultCode==RESULT_OK){
+            ImageUri = data.getData();
+            selectPostImage.setImageURI(ImageUri);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            sendUserBack();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sendUserBack() {
+        Intent intent = new Intent(FindFreelancerActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/kothayebhai/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/kothayebhai/ui/gallery/GalleryFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,16 +16,70 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.kothayebhai.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+    private EditText profileNameEditText, profileEmailEditText, profilePhoneEditText,
+            profileEducationEditText, profileJobEditText, profileNextJobEditText;
+    private Button saveButton;
+    private ProgressBar progressBar;
+
+    private DatabaseReference profileUserRef;
+    private FirebaseAuth mAuth;
+
+    private String currentUserId;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        profileUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+
+        profileNameEditText = root.findViewById(R.id.profileNameEditTextId);
+        profileEmailEditText = root.findViewById(R.id.profileEmailEditTextId);
+        profilePhoneEditText = root.findViewById(R.id.profilePhoneEditTextId);
+        profileEducationEditText = root.findViewById(R.id.profileEducationEditTextId);
+        profileJobEditText = root.findViewById(R.id.profileJobEditTextId);
+        profileNextJobEditText = root.findViewById(R.id.profileNextJobEditTextId);
+
+        profileUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String profileName = dataSnapshot.child("name").getValue().toString();
+                    String profileEmail = dataSnapshot.child("email").getValue().toString();
+                    String profilePhone = dataSnapshot.child("phone").getValue().toString();
+                    String profileEducation = dataSnapshot.child("education").getValue().toString();
+                    String profileJob = dataSnapshot.child("current_job").getValue().toString();
+                    String profileNextJob = dataSnapshot.child("future_job").getValue().toString();
+
+                    profileNameEditText.setText(profileName);
+                    profileEmailEditText.setText(profileEmail);
+                    profilePhoneEditText.setText(profilePhone);
+                    profileEducationEditText.setText(profileEducation);
+                    profileJobEditText.setText(profileJob);
+                    profileNextJobEditText.setText(profileNextJob);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         return root;
     }
