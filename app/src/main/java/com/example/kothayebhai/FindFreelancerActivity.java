7153991bd1da7c38129ed016110cc830a0ae121c
unknown <mousfiqul.khan@northsouth.edu>
2020-02-04 22:07:25 +0600

    Firebase instance for storing image created

--- app/src/main/java/com/example/kothayebhai/FindFreelancerActivity.java
+++ app/src/main/java/com/example/kothayebhai/FindFreelancerActivity.java
@@ -7,13 +7,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class FindFreelancerActivity extends AppCompatActivity {
 
@@ -23,11 +34,18 @@ public class FindFreelancerActivity extends AppCompatActivity {
     private EditText postDescription;
     private static final int Gallery_pick=1;
     private Uri ImageUri;
+    private String description;
+
+    private StorageReference postImagesReference;
+
+    private String saveCurrentDate, saveCurrentTime, postRandomName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_freelancer);
+
+        postImagesReference = FirebaseStorage.getInstance().getReference();
 
         selectPostImage = findViewById(R.id.imageButton4);
         updatePostButton = findViewById(R.id.postButtonId);
@@ -43,6 +61,53 @@ public class FindFreelancerActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openGallery();
+            }
+        });
+
+        updatePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validatePostInfo();
+            }
+        });
+    }
+
+    private void validatePostInfo() {
+        description = postDescription.getText().toString();
+        if(ImageUri == null){
+            Toast.makeText(getApplicationContext(), "Please select an image", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(description)){
+            Toast.makeText(getApplicationContext(), "Please provide description for the job", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            storingImage();
+        }
+    }
+
+    private void storingImage() {
+        Calendar callForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-yyyy");
+        saveCurrentDate = currentDate.format(callForDate.getTime());
+
+        Calendar callForTime = Calendar.getInstance();
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm");
+        saveCurrentTime = currentTime.format(callForTime.getTime());
+
+        postRandomName = saveCurrentDate + saveCurrentTime;
+
+        StorageReference filePath = postImagesReference.child("Post Images").child(ImageUri.getLastPathSegment() + postRandomName + ".jpg");
+
+        filePath.putFile(ImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Image uploaded successfully", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String message = task.getException().getMessage();
+                    Toast.makeText(getApplicationContext(), "Error: " + message, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
